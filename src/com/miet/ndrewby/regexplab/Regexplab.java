a8d9170c73e9c0ce86f13b622d0ad1f4cb767cf9
Andrew By <andrew-by@hotmail.com>
2015-03-25 13:39:25 +0300

    First regexp

--- src/com/miet/ndrewby/regexplab/Regexplab.java
+++ src/com/miet/ndrewby/regexplab/Regexplab.java
@@ -1,10 +1,21 @@
 package com.miet.ndrewby.regexplab;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Andrew on 25.03.2015.
  */
 public class Regexplab {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(removeNumbers(removeNames("Hi, I'm Jack. And today we will talk about Octagon.")));
+    }
+    public static String removeNames(String nameString){
+        return nameString.replaceAll("[^!?.] [A-Z][a-z]+", " [censored]");
+
+    }
+
+    public static String removeNumbers(String nameString){
+        return nameString.replaceAll("^*\\d{7,12}$", "[censored]");
     }
 }
