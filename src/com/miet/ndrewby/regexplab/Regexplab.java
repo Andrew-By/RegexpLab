955a2f334009590d9cc6c790d395fe2dda06dbc3
Andrew By <andrew-by@hotmail.com>
2015-03-25 14:18:58 +0300

    Dictionary added

--- src/com/miet/ndrewby/regexplab/Regexplab.java
+++ src/com/miet/ndrewby/regexplab/Regexplab.java
@@ -1,6 +1,8 @@
 package com.miet.ndrewby.regexplab;
 
 import java.math.MathContext;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,16 +10,24 @@ import java.util.regex.Pattern;
  * Created by Andrew on 25.03.2015.
  */
 public class Regexplab {
+    private static ArrayList<String> names = new ArrayList<String>();
+
     public static void main(String[] args) {
-        System.out.println(removeNumbers(removeNames("Hi, I'm Jack! And today we will talk about Octagon. BTW my phone number is 89261940493!")));
+
+        System.out.println(removeNumbers(removeNamesStage1("Hi, I'm Jack! And today we will talk about Octagon. BTW my phone number is 89261940493!")));
+        System.out.println("Names found:");
+        for(String name : names){
+            System.out.println(name);
+        }
     }
-    public static String removeNames(String nameString){
+    public static String removeNamesStage1(String nameString){
         String out = "";
         Integer lastEnd = 0;
-        Pattern p = Pattern.compile("[^!?.,] [A-Z][^ ][a-z]+");
+        Pattern p = Pattern.compile("[^!?.,] [A-Z][^\\s][a-z]+");
         Matcher m = p.matcher(nameString);
         while(m.find()){
             out+=nameString.substring(lastEnd, m.start()+1)+ " [censored]";
+            names.add(nameString.substring(m.start()+2, m.end()));
             lastEnd = m.end();
         }
         out+=nameString.substring(lastEnd);
