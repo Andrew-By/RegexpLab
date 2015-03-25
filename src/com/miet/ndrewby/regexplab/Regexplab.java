375a78dbac599f979546cb56e710a80a7e5d2a04
Andrew By <andrew-by@hotmail.com>
2015-03-25 12:17:56 +0300

    Initial Commit

---
+++ src/com/miet/ndrewby/regexplab/Regexplab.java
@@ -0,0 +1,10 @@
+package com.miet.ndrewby.regexplab;
+
+/**
+ * Created by Andrew on 25.03.2015.
+ */
+public class Regexplab {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
